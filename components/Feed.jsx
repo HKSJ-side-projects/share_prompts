74590f7d79ed19df93865cc9d04c68d763b9ab75
rohit singh negi <rocknegi53@gmail.com>
2024-02-20 18:49:51 +0100

    show the feed page show all the prompts on the homepage fix an issue: Tag was not visible on the prompt

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect, useState } from 'react'
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+    return (
+        <div className='mt-16 prompt_layout'>
+            {data.map(post => (
+                <PromptCard
+                    key={post.id}
+                    post={post}
+                    handleTagclick={handleTagClick}
+                />
+            ))}
+        </div>
+    )
+}
 
 const Feed = () => {
+    const [searchText, setSearchText] = useState('');
+    const [posts, setPosts] = useState([]);
+
+    const handleSearchChange = () => {
+
+    }
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const respone = await fetch('/api/prompt');
+            const data = await respone.json();
+            setPosts(data);
+        }
+        fetchPosts();
+    }, [])
+
     return (
-        <div>Feed</div>
+        <section className='feed'>
+            <form className='relative w-full flex-center'>
+                <input
+                    className='search_input peer'
+                    type='text'
+                    placeholder='search for a tag or a username'
+                    value={searchText}
+                    onChange={handleSearchChange}
+                    required
+                />
+            </form>
+            <PromptCardList
+                data={posts}
+                handleTagClick={() => { }}
+            />
+        </section>
     )
 }
 
